73bec7557744cf5ab59e0d4a5d1961265c7f8bbd
Pomagierko <sebastianszurgot3@gmail.com>
2026-09-14 23:58:42 +0200

    Initial commit

---
+++ src/main/java/com/pomagierko/tutorial/TutorialMod.java
@@ -0,0 +1,13 @@
+package com.pomagierko.tutorial;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "tm", name = "TutorialMod", version = "1.0")
+public class TutorialMod {
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        //Item/Block init and registering
+        //Config handling
+    }
+}
