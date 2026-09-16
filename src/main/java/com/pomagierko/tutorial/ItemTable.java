6f2745e1fc444b6b2834052870a2953f2b05ada3
Pomagierko <sebastianszurgot3@gmail.com>
2026-09-17 00:02:03 +0200

    added a block

---
+++ src/main/java/com/pomagierko/tutorial/ItemTable.java
@@ -0,0 +1,6 @@
+package com.pomagierko.tutorial;
+
+import net.minecraft.item.Item;
+
+public class ItemTable extends Item {
+}
--- src/main/java/com/pomagierko/tutorial/TutorialMod.java
+++ src/main/java/com/pomagierko/tutorial/TutorialMod.java
@@ -1,13 +1,42 @@
 package com.pomagierko.tutorial;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.Item;
 
 @Mod(modid = "tm", name = "TutorialMod", version = "1.0")
 public class TutorialMod {
+
+    public static Item itemTable;
+    public static Block blockTable;
+
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         //Item/Block init and registering
         //Config handling
+        itemTable = new ItemTable().setUnlocalizedName("ItemTable").setTextureName("tm:itemtable");
+
+        blockTable = new BlockTable(Material.wood).setBlockName("BlockTable").setBlockTextureName("tm:blocktable");
+
+
+
+        GameRegistry.registerItem(itemTable, itemTable.getUnlocalizedName().substring(5));
+        GameRegistry.registerBlock(blockTable, blockTable.getUnlocalizedName().substring(5));
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        //Proxy, TileEntity, entity, GUI and Packet Registering
+
+    }
+
+    @Mod.EventHandler
+    public void postinit(FMLPostInitializationEvent event) {
+
     }
 }
