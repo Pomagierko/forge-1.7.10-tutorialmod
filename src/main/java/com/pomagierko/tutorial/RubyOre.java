15bd35a54e3199f1c36c6bfb98b91b972048ac35
Pomagierko <sebastianszurgot3@gmail.com>
2026-09-21 00:22:28 +0200

    added cheese a food item, which can be obtained thorugh smeling milk in the oven, you dont get the bucket back though. Plus a creative tab

---
+++ src/main/java/com/pomagierko/tutorial/RubyOre.java
@@ -0,0 +1,10 @@
+package com.pomagierko.tutorial;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class RubyOre extends Block {
+    public RubyOre(Material material) {
+        super(material);
+    }
+}
--- src/main/java/com/pomagierko/tutorial/TutorialMod.java
+++ src/main/java/com/pomagierko/tutorial/TutorialMod.java
@@ -7,36 +7,56 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemFood;
+import net.minecraft.item.ItemStack;
 
 @Mod(modid = "tm", name = "TutorialMod", version = "1.0")
 public class TutorialMod {
 
     public static Item itemTable;
+    public static Item itemCheese;
     public static Block blockTable;
+    public static Block rubyOre;
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         //Item/Block init and registering
         //Config handling
-        itemTable = new ItemTable().setUnlocalizedName("ItemTable").setTextureName("tm:itemtable");
+        itemTable = new ItemTable().setUnlocalizedName("ItemTable").setTextureName("tm:itemtable").setCreativeTab(tabTutorialMod);
+        itemCheese = new ItemFood(6, 3.0F, false).setUnlocalizedName("ItemCheese").setTextureName("tm:itemcheese").setCreativeTab(tabTutorialMod);
 
-        blockTable = new BlockTable(Material.wood).setBlockName("BlockTable").setBlockTextureName("tm:blocktable");
+        blockTable = new BlockTable(Material.wood).setBlockName("BlockTable").setBlockTextureName("tm:blocktable").setCreativeTab(tabTutorialMod);
+        rubyOre = new RubyOre(Material.rock).setBlockName("RubyOre").setBlockTextureName("tm:rubyore").setCreativeTab(tabTutorialMod);
 
 
 
         GameRegistry.registerItem(itemTable, itemTable.getUnlocalizedName().substring(5));
+        GameRegistry.registerItem(itemCheese, itemCheese.getUnlocalizedName().substring(5));
         GameRegistry.registerBlock(blockTable, blockTable.getUnlocalizedName().substring(5));
+        GameRegistry.registerBlock(rubyOre, rubyOre.getUnlocalizedName().substring(5));
     }
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
         //Proxy, TileEntity, entity, GUI and Packet Registering
-
+        GameRegistry.addRecipe(new ItemStack(itemTable, 2), new Object[]{"###","# #","# #", '#', Blocks.planks});
+        GameRegistry.addRecipe(new ItemStack(blockTable), new Object[]{"###","#R#","# #", '#', Blocks.planks, 'R', Blocks.redstone_block});
+        GameRegistry.addSmelting(Items.milk_bucket, new ItemStack(itemCheese), 3.0f);
     }
 
     @Mod.EventHandler
     public void postinit(FMLPostInitializationEvent event) {
 
     }
+
+    public static CreativeTabs tabTutorialMod = new CreativeTabs("tabTutorialMod") {
+        @Override
+        public Item getTabIconItem() {
+            return new ItemStack(itemCheese).getItem();
+        }
+    };
 }
